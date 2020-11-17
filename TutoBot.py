35c96488c226b0e265249a03b0a7a37d50ebe43b
neiviv-dev <60667857+neiviv-dev@users.noreply.github.com>
2020-11-17 17:28:27 +0100

    Add files via upload

---
+++ TutoBot.py
@@ -0,0 +1,71 @@
+import discord
+
+from discord.ext import commands
+
+TOKEN = 'Nzc3NjI5MjU3ODQ4ODQ4NDU1.X7GNrg.WKZM_QHY2ZIjb3xE1hD8N4Kva28'
+
+client = commands.Bot(command_prefix = '.')
+client.remove_command('help')
+
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+    await client.change_presence(activity=discord.Game(name='Kill Every Human Being'))
+
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    print(author,content)
+    await client.process_commands(message)
+
+@client.event
+async def on_message_delete(message):
+    author = message.author
+    content= message.content
+    channel = message.channel
+    if content[0] != '.':
+        await channel.send('C\'est pas bien de supprimer {} :rage:'.format(author))
+    else:
+        print(content)
+    
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong!')
+
+@client.command(pass_context=True)
+async def clear(ctx, amount):
+    channel = ctx.message.channel
+    messages = []
+    async for message in channel.history(limit = int(amount)):
+        messages.append(message)
+    await channel.delete_messages(messages)
+    await channel.send("{} messages supprimés".format(amount))
+
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+    channel = ctx.message.channel
+    embed = discord.Embed(
+        colour = discord.Color.purple()
+    )
+
+    embed.set_author(name = 'Help')
+    embed.add_field(name = ".ping", value = 'Renvoie Pong!', inline=False)
+    embed.add_field(name = ".clear <nombre>", value = 'Supprime <nombre> messages du salon', inline=False)
+    embed.add_field(name = ".trash", value = 'Envoie une insulte random', inline=False)
+    embed.add_field(name = ".compliment", value = 'Envoie une insulte random', inline=False)
+
+    await author.send(embed=embed)
+
+    messages = []
+    async for message in channel.history(limit = 1):
+        messages.append(message)
+    await channel.delete_messages(messages)
+
+
+
+
+client.run(TOKEN)
